3009c3a404c87d0f585d32fae48decbe9856d557
Terence Lawson <Terence.Lawson1532@gmail.com>
2025-08-14 15:31:02 +0100

    fix pictures

--- src/Nav.tsx
+++ src/Nav.tsx
@@ -6,7 +6,7 @@ function Nav() {
 
   return (
     <nav className="w-full py-6 px-4 bg-slate-950/80 justify-center fixed top-0 left-0 z-50 shadow flex items-center">
-      <Link to="/YarmBuildingSolutions/" className="text-2xl mr-32 font-bold text-brand-gold">Yarm Building Solutions</Link>
+      <Link to="/" className="text-2xl mr-32 font-bold text-brand-gold">Yarm Building Solutions</Link>
       <button
         className="md:hidden text-brand-gold text-3xl focus:outline-none"
         onClick={() => setOpen(!open)}
